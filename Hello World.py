5a79af7ac10318fb2366234744e93f1493e9ba6b
IFICL <291474043@qq.com>
2017-08-25 23:53:17 +0800

    learn print

--- Hello World.py
+++ Hello World.py
@@ -1,1 +1,11 @@
-print 'hello world'
+#!/usr/bin/python3.5
+#Filename:Hello World.py
+print("Hello", end = " ")#only in python3.x
+x = "World"
+print(x,"!", sep = '', end ="\n") #only in python3.x
+y = '''first line
+second line
+third line'''
+print(y)
+z = 'connect' 'ed'
+print(z)
