7a3e7785d549ecf684f84c4571e3d447217c5d41
IFICL <291474043@qq.com>
2017-08-30 16:29:33 +0800

    learn string

--- Hello World.py
+++ Hello World.py
@@ -3,9 +3,17 @@
 print("Hello", end = " ")#only in python3.x
 x = "World"
 print(x,"!", sep = '', end ="\n") #only in python3.x
+
 y = '''first line
 second line
 third line'''
 print(y)
+#using '''xxx''' to get the string has several lines
+
 z = 'connect' 'ed'
 print(z)
+#python will connected the those string.
+
+a = r"Blank line \n" 
+print(a)
+#Add 'R' or 'r' to make the signs as the it looks like.
