f7dcf4d57848419f4f3f1cfcc4113d8fe3d011dd
IFICL <291474043@qq.com>
2018-03-07 13:54:58 +0800

    3.7

---
+++ TensorFlow/fetch.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+#Filename:fetch.py
+
+import tensorflow as tf 
+
+input1 = tf.constant(3.0)
+input2 = tf.constant(2.0)
+input3 = tf.constant(5.0)
+intermed = tf.add(input2, input3)
+mul = tf.mul(input1, intermed)
+
+with tf.Session() as sess:
+  result = sess.run([mul, intermed])
+  print result
