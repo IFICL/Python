d28a9ca2ed2ea8a81ece2630acd3d5a21f325f67
IFICL <291474043@qq.com>
2018-03-07 10:51:15 +0800

    new

--- TensorFlow/test.py
+++ TensorFlow/test.py
@@ -1,1 +1,12 @@
-Linking Start!
+#!/usr/bin/python2.7
+#Filename:while.py
+
+import tensorflow as tf
+
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print sess.run(hello)
+
+a = tf.constant(10)
+b = tf.constant(32)
+print sess.run(a + b)
